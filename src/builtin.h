5af7f9eb381592117db9214405872145f288f7be
eliss <staaf.elis@gmail.com>
2024-11-09 19:54:05 +0100

    Separate write() and writeln() to be different

--- src/builtin.h
+++ src/builtin.h
@@ -47,6 +47,23 @@ obj_t *stdlib_decl_write(ctx_t *ctx, size_t count, obj_t *args[])
         return NULL;
     }
 
+    printf("%s", ((obj_str_t*)args[0])->str);
+    return NULL;
+}
+
+obj_t *stdlib_decl_writeln(ctx_t *ctx, size_t count, obj_t *args[])
+{
+    if(count != 1)
+    {
+        stdlib_error_arg_count("writeln()", 1, count);
+        return NULL;
+    }
+    if(args[0]->type != ot_str)
+    {
+        stdlib_error_arg_type("writeln()", ot_str, args[0]->type);
+        return NULL;
+    }
+
     printf("%s\n", ((obj_str_t*)args[0])->str);
     return NULL;
 }
@@ -55,6 +72,7 @@ void stdlib_context(ctx_t *ctx)
 {
     if(!ctx) return;
     add_var(ctx, create_var("write", create_nat_obj(ctx, stdlib_decl_write), false));
+    add_var(ctx, create_var("writeln", create_nat_obj(ctx, stdlib_decl_writeln), false));
     add_var(ctx, create_var("tostring", create_nat_obj(ctx, stdlib_decl_tostring), false));
 }
 
